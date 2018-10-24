cc857273a476df946c1c52d3ae64f5014ece084c
Nadia Lee <miss.nadia.lee@gmail.com>
2018-10-24 23:48:52 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -14,5 +14,13 @@ class RepositoriesController < ApplicationController
   end
 
   def create
+    resp = Faraday.post("https://api.github.com/user/repos") do |req|
+      req.body = { 'client_id': client_id, 'client_secret': client_secret, 'code': code }
+      req.headers['Accept'] = 'application/json'
+    end
+
+    body = JSON.parse(resp.body)
+    session[:token] = body['access_token']
+    redirect_to root_path
   end
 end
