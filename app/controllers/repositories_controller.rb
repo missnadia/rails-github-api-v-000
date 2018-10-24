d8ea4408bc087e7c861d2d3175ba746f0cbd129e
Nadia Lee <miss.nadia.lee@gmail.com>
2018-10-24 23:32:11 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,11 @@
 class RepositoriesController < ApplicationController
-  
   def index
+    resp = Faraday.get("https://api.github.com/user") do |req|
+
+    end
   end
 
+  def create
+    redirect_to '/'
+  end
 end
