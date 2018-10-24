8ea7a3c49293e58b5bc36722ea90103b0878d8c9
Nadia Lee <miss.nadia.lee@gmail.com>
2018-10-24 23:47:22 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -4,8 +4,14 @@ class RepositoriesController < ApplicationController
       req.headers['Authorization'] = 'token ' + session[:token]
       req.headers['Accept'] = 'application/json'
     end
-
     @user_data = JSON.parse(user.body)
+
+    repo = Faraday.get("https://api.github.com/user/repos") do |req|
+      req.headers['Authorization'] = 'token ' + session[:token]
+      req.headers['Accept'] = 'application/json'
+    end
+
+    @repo_data = JSON.parse(repo.body)
   end
 
   def create
