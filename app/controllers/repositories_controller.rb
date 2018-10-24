84285e23784102293ec1910ea0f6ae968b595784
Nadia Lee <miss.nadia.lee@gmail.com>
2018-10-24 23:41:33 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,9 +1,11 @@
 class RepositoriesController < ApplicationController
   def index
-    Faraday.get("https://api.github.com/user") do |req|
+    user = Faraday.get("https://api.github.com/user") do |req|
       req.headers['Authorization'] = 'token ' + session[:token]
       req.headers['Accept'] = 'application/json'
     end
+
+    @user_data = JSON.parse(user.body)
   end
 
   def create
